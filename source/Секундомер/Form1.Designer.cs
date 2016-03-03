a42aaa6cb091f582a4df4955d1e1704cc7b84858
ladone <mypmeow@gmail.com>
2016-03-03 20:06:12 +0200

    pull version 0.1

---
+++ source/Секундомер/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TimeWorkCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.about = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Location = new System.Drawing.Point(117, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(470, 215);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "History Box";
+            // 
+            // about
+            // 
+            this.about.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.about.Location = new System.Drawing.Point(335, 9);
+            this.about.Name = "about";
+            this.about.ReadOnly = true;
+            this.about.Size = new System.Drawing.Size(147, 85);
+            this.about.TabIndex = 5;
+            this.about.Text = "Created by:\nVlad \"Ladone\" Pototckiy\nE-mail: mypmeow@gmail.com\n\nVersion: 0.01";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(335, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear history";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(494, 345);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.about);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Work Time Counter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox about;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ source/Секундомер/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TimeWorkCounter
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        Stopwatch stopwatch = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MaximizeBox = false;
+            
+            timer.Interval = 10;
+            timer.Start();
+            timer.Tick += new EventHandler(tickTimer);
+            richTextBox1.ReadOnly = true;
+            richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void tickTimer(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopwatch.Elapsed;
+            /*string elepsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                                            ts.Hours, ts.Minutes, ts.Seconds,
+                                                       ts.Milliseconds / 10);*/
+            label1.Text = stopwatch.Elapsed.ToString(@"hh\:mm\:ss\.ff");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text != "00:00:00.00" && button1.Text != "Resume")
+            {
+                stopwatch.Stop();
+                button1.Text = "Resume";
+                richTextBox1.Text += DateTime.Now.ToString(@"HH:mm:ss") + ": Pause.\nWork current time: " + label1.Text + "\n";
+            }
+            else
+            {
+                stopwatch.Start();
+                button1.Text = "Pause";
+                richTextBox1.Text += DateTime.Now.ToString(@"HH:mm:ss") + ": Resume.\nWork current time: " + label1.Text + "\n";
+            }
+            //stopwatch.Reset();
+           // stopwatch.Start();
+         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+            button1.Text = "Start";
+            richTextBox1.Text += DateTime.Now.ToString(@"HH:mm:ss") + ": Stop.\nWork current time: " + label1.Text + "\n";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = DateTime.Now.ToString(@"HH:mm:ss");
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
